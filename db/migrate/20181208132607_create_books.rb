6d8477373d8a6f9f25da670eeb16c8e9b4c34047
moroo0308 <moroo0308@gmail.com>
2018-12-08 13:34:08 +0000

    [Add]all model

---
+++ db/migrate/20181208132607_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.integer :user_id
+      t.string :article
+      t.string :url
+      t.text :comment
+      t.string :image_id
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
