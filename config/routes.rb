64f39a189875e05a780584191f1634abdbf403e2
moroo0308 <moroo0308@gmail.com>
2018-12-10 02:25:17 +0000

    [Add]books_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/destroy'
-  get 'users/update'
   get 'searches/search'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'book_comments/create'
-  get 'book_comments/destroy'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/create'
-  get 'books/update'
-  get 'books/destroy'
   devise_for :users
+  resources :users, only: [:show,:edit,:update,:destroy]
+  resources :books
+  resources :book_comments, only: [:create,:destroy]
+  resources :favorites, only: [:create,:destroy]
   root "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
