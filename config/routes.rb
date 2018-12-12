e314985efce1b634fdd5cd8303bd4843c3b25eab
moroo0308 <moroo0308@gmail.com>
2018-12-12 17:33:46 +0000

    [Add]books_index,show users_show

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   post "favorites/:book_id/create" => "favorites#create"
   post "favorites/:book_id/destroy" => "favorites#destroy"
+
+  get "users/:id/favorites" => "users#favorites"
   root "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
