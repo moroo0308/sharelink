1628fcff44d354cd73c1d87f275860496bd09bbd
moroo0308 <moroo0308@gmail.com>
2018-12-22 00:52:36 +0000

    [Add][css]books,users change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -117,6 +117,25 @@ $(function() {
 });
 
 
+var option = {
+  section : '.js-section', // 対象を指定
+  easing: "swing", // イージングをしてい(jQueryのanimation)
+  scrollSpeed: 600, // スクロール時の速度
+  scrollbars: true, // スクロールバーを表示するか
+};
+
+$(function() {
+  $.scrollify(option); // scrollifyの実行
+});
+
+
+
+
+$(window).load(function(){
+  $('.loading').fadeOut();  
+});
+
+
 // $(function(){
 //   $('.like_btn').on('click',function(){
 //     var $likeBtn = $(this);
