251413e3a3568df02f6d1677ebae1599440bc1ce
moroo0308 <moroo0308@gmail.com>
2019-01-15 06:47:54 +0000

    [deploy][Add]db/migrate/

--- db/migrate/20181208132607_create_books.rb
+++ db/migrate/20181208132607_create_books.rb
@@ -1,6 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
-    create_table :books do |t|
+    create_table :books, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :user_id
       t.string :article
       t.string :url
